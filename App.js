01552558358eccf10e8a8aa7788a9ba8d469d388
RammyMcRamster <82988557+RammyMcRamster@users.noreply.github.com>
2023-11-25 08:23:37 -0500

    Adding actual substance
    html and js files. should use live share extension on vscode.

--- App.js
+++ App.js
@@ -1,3 +1,19 @@
+<<<<<<< Updated upstream
 // Filename - App.js
 console.log("run js")
 console.log(1+4)
+=======
+import React from 'react'
+
+
+<html>
+    <h1>Hello</h1>
+    <script>
+        alert("js message");
+    </script>
+    <button onclick = "website()">
+        easesearchlogo.png
+    </button>
+    
+</html>
+>>>>>>> Stashed changes
